2deb01e4a29853ba198bcb9908c3dba9b7bfbbe4
cherch <2196047@slu.edu.ph>
2020-09-23 21:43:17 +0800

    Completion of code for the Calculator app

--- main.py
+++ main.py
@@ -22,3 +22,25 @@ print("1.Addition")
 print("2.Subtraction")
 print("3.Multiplication")
 print("4.Division")
+
+while True:
+    operator = input("Enter choice(A/B/C/D): ")
+
+    if operator in ('A', 'B', 'C', 'D'):
+        num1 = float(input("Enter first number: "))
+        num2 = float(input("Enter second number: "))
+
+        if operator == 'A':
+            print(num1, "+", num2, "=", add(num1, num2))
+
+        elif operator == 'B':
+            print(num1, "-", num2, "=", subtract(num1, num2))
+
+        elif operator == 'C':
+            print(num1, "*", num2, "=", multiply(num1, num2))
+
+        elif operator == 'D':
+            print(num1, "/", num2, "=", divide(num1, num2))
+        break
+    else:
+        print("Invalid Input")
